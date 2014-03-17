a3f5c5d1b346bac8108fc0b22871991272b44de0
Annica Alstermark <annica.alstermark@gmail.com>
2014-03-17 13:18:16 +0100

    Created layout for login screen.

--- client/Lister/Lister/src/main/java/se/dxtr/lister/MainActivity.java
+++ client/Lister/Lister/src/main/java/se/dxtr/lister/MainActivity.java
@@ -1,9 +1,11 @@
 package se.dxtr.lister;
 
 import android.app.Activity;
+import android.app.Application;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
