8a8465c9bcb26156ddfe8b704e566942a9c40cec
Annica Alstermark <annica.alstermark@gmail.com>
2014-03-20 11:52:57 +0100

    Implemented login functionality and started working on the dynamic loading of the lists.

--- client/Lister/Lister/src/main/java/se/dxtr/lister/controller/LoginViewController.java
+++ client/Lister/Lister/src/main/java/se/dxtr/lister/controller/LoginViewController.java
@@ -1,16 +1,35 @@
 package se.dxtr.lister.controller;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 import se.dxtr.lister.CreateAccountActivity;
 import se.dxtr.lister.ListOverviewActivity;
+import se.dxtr.lister.model.ListData;
 import se.dxtr.lister.model.ListerModel;
 import se.dxtr.lister.view.LoginView;
 import se.dxtr.lister.R;
 
 public class LoginViewController implements OnClickListener {
     LoginView view;
+    EditText usernameField, passwordField;
     private ListerModel model;
 
     public LoginViewController(LoginView view, ListerModel model) {
@@ -26,14 +45,149 @@ public class LoginViewController implements OnClickListener {
 
         switch (v.getId()) {
             case R.id.login_button:
-                intent = new Intent(view.activity, ListOverviewActivity.class);
-                model.testRequest();
-                view.activity.startActivity(intent);
+                usernameField = (EditText) view.activity.findViewById(R.id.username);
+                passwordField = (EditText) view.activity.findViewById(R.id.password);
+
+                String username = usernameField.getText().toString();
+                String password = passwordField.getText().toString();
+
+                new LoginTask().execute(username, password);
                 break;
             case R.id.create_account:
                 intent = new Intent(view.activity, CreateAccountActivity.class);
                 view.activity.startActivity(intent);
                 break;
+        }
+    }
+
+    public String validateLogin(String name, String password) {
+        URL host = null;
+        InputStream is = null;
+        try {
+            host = new URL("http://130.229.174.119:8888/login/name=" + name + "&password=" +password);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try {
+            HttpURLConnection conn = (HttpURLConnection) host.openConnection();
+            conn.setReadTimeout(15000 /* milliseconds */);
+            conn.setConnectTimeout(5000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            if (response != 200) {
+                return "False";
+            }
+            Log.d("Server", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            br.close();
+
+            return sb.toString();
+
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "False";
+    }
+
+    private class LoginTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String name = params[0];
+            String password = params[1];
+            return validateLogin(name, password);
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            if (response.equals("False")) {
+                Context context = view.activity.getApplicationContext();
+                CharSequence text = "Wrong username or password";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+            } else {
+                new FetchListTask().execute(response);
+            }
+            Log.d("The response: ", response);
+        }
+    }
+
+    public String fetchLists(String uid) {
+        URL host = null;
+        InputStream is = null;
+        try {
+            host = new URL("http://130.229.174.119:8888/get_lists/id="+uid);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try {
+            HttpURLConnection conn = (HttpURLConnection) host.openConnection();
+            conn.setReadTimeout(15000 /* milliseconds */);
+            conn.setConnectTimeout(5000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            if (response != 200) {
+                return "False";
+            }
+            Log.d("Server", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            br.close();
+
+            return sb.toString();
+
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "False";
+
+    }
+
+    private class FetchListTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String uid = params[0];
+            return fetchLists(uid);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Intent intent;
+
+            Log.d("Result", result);
+            Gson gson = new Gson();
+            ListData listData = gson.fromJson(result, ListData.class);
+            model.loadListData(listData);
+
+            intent = new Intent(view.activity, ListOverviewActivity.class);
+            view.activity.startActivity(intent);
         }
     }
 
--- client/Lister/Lister/src/main/java/se/dxtr/lister/view/ListOverviewView.java
+++ client/Lister/Lister/src/main/java/se/dxtr/lister/view/ListOverviewView.java
@@ -1,25 +1,42 @@
 package se.dxtr.lister.view;
 
 import se.dxtr.lister.R;
+import se.dxtr.lister.model.ListerModel;
+import se.dxtr.lister.model.TodoList;
+
 import android.app.Activity;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
+import java.util.List;
 
 public class ListOverviewView {
     View view;
+    ListerModel model;
     public final Activity activity;
-    public LinearLayout overviewElementId;
+    public LinearLayout overviewElement;
 
-    public ListOverviewView(View view, final Activity activity) {
+    public ListOverviewView(View view, ListerModel model, final Activity activity) {
 
         // store in the class the reference to the Android View
         this.view = view;
         this.activity = activity;
+        this.model = model;
 
         // Setup the rest of the view layout
+        List<TodoList> todoLists = model.getTodoLists();
+        ScrollView overviewContainer = (ScrollView) activity.findViewById(R.id.overview_container);
+        for (TodoList todoList : todoLists) {
+            TextView listTitle = (TextView) activity.findViewById(R.id.list_title);
+            listTitle.setText(todoList.getTitle());
+        }
 
-        overviewElementId = (LinearLayout) activity.findViewById(R.id.overview_element_id);
+
+        overviewElement = (LinearLayout) activity.findViewById(R.id.overview_element_id);
+
+
     }
 
 }
