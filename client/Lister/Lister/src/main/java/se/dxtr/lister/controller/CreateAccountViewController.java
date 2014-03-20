119e6d3ede6798378f5628dc2a428ee8ecc14dd9
Dexter Gramfors <dexter.gramfors@gmail.com>
2014-03-20 18:11:33 +0100

    Added functionality for creating an account.

--- client/Lister/Lister/src/main/java/se/dxtr/lister/controller/CreateAccountViewController.java
+++ client/Lister/Lister/src/main/java/se/dxtr/lister/controller/CreateAccountViewController.java
@@ -1,14 +1,18 @@
 package se.dxtr.lister.controller;
 
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import se.dxtr.lister.R;
+import se.dxtr.lister.ResponseReader;
 import se.dxtr.lister.view.CreateAccountView;
 
 public class CreateAccountViewController implements OnClickListener {
@@ -22,20 +26,53 @@ public class CreateAccountViewController implements OnClickListener {
 
     @Override
     public void onClick(View v) {
-        EditText username = (EditText) view.activity.findViewById(R.id.created_username);
-        EditText password = (EditText) view.activity.findViewById(R.id.created_password);
+        String username = ((EditText) view.activity.findViewById(R.id.created_username))
+                .getText().toString();
+        String password = ((EditText) view.activity.findViewById(R.id.created_password))
+                .getText().toString();
 
-        view.activity.finish();
+        if (username.length() < 3 || password.length() < 3) {
+            Context context = view.activity.getApplicationContext();
+            CharSequence text = "Password or username is too short";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }else {
+            new createAccountTask().execute(username, password);
+        }
+
     }
 
-    private void createAccount(String username, String password) {
+    private String createAccount(String username, String password) {
         URL host = null;
-        InputStream is = null;
         try {
-            host = new URL(view.activity.getString(R.string.host) + "create_account/username="
+            host = new URL(view.activity.getString(R.string.host) + "create_account/name="
                     + username + "&password=" + password);
         } catch (MalformedURLException e) {
             e.printStackTrace();
+        }
+        return ResponseReader.getResponse(host);
+    }
+
+    private class createAccountTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String username = params[0];
+            String password = params[1];
+            return createAccount(username, password);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d("Create account response", result);
+            Context context = view.activity.getApplicationContext();
+            CharSequence text = "Account created!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            view.activity.finish();
         }
     }
 
